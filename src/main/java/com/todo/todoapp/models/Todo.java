a6445d647660fa3486112012136f5abc5b89deec
andrew21-mch <nfonandrew73@gmail.com>
2022-05-05 10:50:55 +0100

    updated model structutes

--- src/main/java/com/todo/todoapp/models/Todo.java
+++ src/main/java/com/todo/todoapp/models/Todo.java
@@ -1,63 +1,32 @@
 package com.todo.todoapp.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sun.istack.NotNull;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Table
 public class Todo {
 
-    private Long Id;
-
-    @NotNull
-    private String title;
-    private String description;
-    private boolean done;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("id")
     private Long id;
 
-    public Todo(){
-
-    }
-    public Todo(Long Id, String title,String description, boolean done){
-        this.Id = Id;
-        this.title = title;
-        this.description = description;
-        this.done = done;
-    }
-
-    @Id
-    @GeneratedValue
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public boolean isDone() {
-        return done;
-    }
-
-    public void setDone(boolean done) {
-        this.done = done;
-    }
+    @NotNull @JsonProperty("title")
+    private String title;
+    @JsonProperty("desc")
+    private String description;
+    @JsonProperty("done")
+    private Boolean done;
 
 }
